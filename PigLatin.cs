12a79f5f7d41d8059a70beb072b2cb2704b9230e
JamesLynch01 <60361270+JamesLynch01@users.noreply.github.com>
2020-04-12 15:32:04 -0500

    Add files via upload

---
+++ PigLatin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            Console.WriteLine("Enter a word please: ");
+            string pig = Console.ReadLine();
+            int firstVowel = pig.ToLower().IndexOfAny(vowels);
+            int lastVowel = pig.ToLower().IndexOfAny(vowels, pig.Length -1);
+            string firstLetter = pig.ToLower().Substring(0, 1);
+            string secondLetter = pig.ToLower().Substring(1, pig.Length -1);
+            //bool isFirstLetterVowel = false;
+            //bool isLastLetterVowel = false;
+            if (lastVowel == pig.Length -1 && firstVowel == 0)
+            {
+                Console.WriteLine(pig + "yay");
+            }
+            else if (lastVowel != pig.Length -1 && firstVowel == 0)
+            {
+                Console.WriteLine(pig + "ay");
+            }
+            else if (firstVowel > 0)
+            {
+                Console.WriteLine(secondLetter + firstLetter + "ay");
+            }
+            else
+            {
+                Console.WriteLine(pig + "ay");
+            }
+            
+        }
+    }
+}
---
+++ RPSgame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RockPaperScissorGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int computerHand = random.Next(0, 3);
+
+            Console.WriteLine("Type rock, paper or scissors");
+            string answer = Console.ReadLine();
+            //string answer = "rock";
+            int convertAnswer = 0;
+            if (answer.ToLower() == "rock") { convertAnswer = 0; }
+            else if (answer.ToLower() == "paper") { convertAnswer = 1; }
+            else if (answer.ToLower() == "scissors") { convertAnswer = 2; }
+
+            string[] hands = {"rock", "paper", "scissors"};
+            Console.WriteLine("Computer choose: " + hands[computerHand]);
+            Console.WriteLine("Player choose: " + hands[convertAnswer]);
+
+            if (hands[computerHand] == "rock" && answer.ToLower() == "rock") { Console.WriteLine("draw"); }
+            else if (hands[computerHand] == "rock" && answer.ToLower() == "scissors") { Console.WriteLine("computer wins"); }
+            else if (hands[computerHand] == "rock" && answer.ToLower() == "paper") { Console.WriteLine("player wins"); }
+            else if (hands[computerHand] == "paper" && answer.ToLower() == "rock") { Console.WriteLine("computer wins"); }
+            else if (hands[computerHand] == "paper" && answer.ToLower() == "scissors") { Console.WriteLine("player wins"); }
+            else if (hands[computerHand] == "paper" && answer.ToLower() == "paper") { Console.WriteLine("draw"); }
+            else if (hands[computerHand] == "scissors" && answer.ToLower() == "rock") { Console.WriteLine("player wins"); }
+            else if (hands[computerHand] == "scissors" && answer.ToLower() == "scissors") { Console.WriteLine("draw"); }
+            else if (hands[computerHand] == "scissors" && answer.ToLower() == "paper") { Console.WriteLine("computer wins"); }
+             
+        }
+
+        //static String CompareHands(String hand1, String hand2);
+    }
+}
