d6641f566ee4cf45b7af0ccadbd99dedfff27db7
JamesLynch01 <60361270+JamesLynch01@users.noreply.github.com>
2020-04-19 14:42:18 -0500

    Add files via upload

---
+++ Mastermind.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = new string[] { "red", "yellow", "blue" };
+            List<string> userGuess = new List<string>();
+            List<string> computerAnswer = new List<string>();
+            Random color = new Random();
+
+            computerAnswer.Add(colors[color.Next(0, 3)]);
+            computerAnswer.Add(colors[color.Next(0, 3)]);
+
+            bool isPlaying = true;
+            while(isPlaying)
+            {
+                Console.WriteLine("Welcome to Mastermind, can you out think a computer?");
+                Console.WriteLine("Please pick colors from red, blue or yellow. You can pick repeats.");
+                userGuess.Add(Console.ReadLine().ToLower());
+                Console.WriteLine("Please pick another color between red, blue, or yellow.");
+                userGuess.Add(Console.ReadLine().ToLower());
+
+                Console.WriteLine($"{computerAnswer[0]} - {computerAnswer[1]}");
+
+                if (userGuess[0].Equals(computerAnswer[0]) && userGuess[1].Equals(computerAnswer[1]))
+                {
+                    Console.WriteLine("You guessed right. Guess you are smarter than a computer.");
+                    break;
+                }
+                else if (userGuess[0].Equals(computerAnswer[0]) || userGuess[1].Equals(computerAnswer[1]))
+                {
+                    Console.WriteLine("\n0 - 1. You guessed one of the colors in the correct position.");
+                }
+                else if (userGuess.Contains(computerAnswer[0]) || userGuess.Contains(computerAnswer[1]))
+                {
+                    if (userGuess[0].Equals(computerAnswer[1]) && userGuess[1].Equals(computerAnswer[0]))
+                    {
+                        Console.WriteLine("\n2 - 0. You guessed both of the colors but in the wrong order.");
+                    } 
+                    else
+                    {
+                        Console.WriteLine("\n1 - 0. You guessed one of the colors correctly but not in correct position.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n0 - 0. You guess wrong on both accounts, try again.");
+                }
+            }
+        }
+
+    }
+}
---
+++ gradeBook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace gradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            string student_grades = string.Empty;
+            Dictionary<string, string> gradeBook = new Dictionary<string, string>();
+
+            do
+            {
+                Console.WriteLine("Please enter a student name. When done enter 'quit' in names slot");
+                name = Console.ReadLine().ToLower();
+                if (name.Equals("quit"))
+                    break;
+
+                Console.WriteLine("Enter the grades for your student in one line with spaces.");
+                student_grades = Console.ReadLine();
+
+                gradeBook.Add(name, student_grades);
+
+                
+
+            } while (true);
+
+            int bestGrade = 0;
+            int worstGrade = 0;
+            double averageGrade = 0.0;
+
+
+            foreach (var item in gradeBook)
+            {
+                int[] grades = Array.ConvertAll<string, int>(gradeBook[item.Key].Split(), Convert.ToInt32);
+
+                bestGrade = grades.Max();
+                worstGrade = grades.Min();
+                averageGrade = grades.Average();
+                Console.WriteLine($"{item.Key}\n");
+                Console.WriteLine($"Highest Grades = {bestGrade} lowest Grades = {worstGrade} Average = {averageGrade}");
+            }
+            
+        }
+    }
+}
