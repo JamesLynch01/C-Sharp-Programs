122aa3f0f41e7e19c1a7147e755a15d5f12fb7dc
JamesLynch01 <60361270+JamesLynch01@users.noreply.github.com>
2020-04-26 08:42:50 -0500

    Add files via upload

---
+++ PoCos.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace PoCos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            License Jill = new License("Jill", "Larry");
+            Jill.LicenseNumber = 13;
+            Jill.Gender = "female";
+            
+            Console.WriteLine("{0} is a {1} with this number {2}",
+                Jill.FullName(), Jill.Gender, Jill.LicenseNumber
+                );
+
+            Book Ring = new Book("Tolkien", "Ring Dancing");
+           
+            Ring.PagesBook = 560;
+            Ring.PriceBook = 8;
+            Ring.SKU = 839230875;
+            Ring.PublisherBook = "the Royal Library";
+            Ring.BuyBook();
+
+
+
+
+            Airplanes B3 = new Airplanes("Boeing", "B-17 Flying Fortress", "XB-38 flying fortress", 10, 4);
+            
+            B3.AirShow();
+           
+        }
+    }
+
+    public class License
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int LicenseNumber { get; set; }
+       
+
+        public License(string initialFirstName, string initialSecondName)
+        {
+            this.FirstName = initialFirstName;
+            this.LastName = initialSecondName;
+            
+        }
+
+        public string FullName()
+        {
+            string fullName = FirstName + " " + LastName;
+            return fullName;
+        }
+    }
+
+    public class Book
+    {
+        public string TitleBook;
+        public string AuthorsBook;
+        public int PagesBook;
+        public int SKU;
+        public string PublisherBook;
+        public int PriceBook;
+
+        public Book(string authorsBook, string titleBook)
+        {
+            this.AuthorsBook = authorsBook;
+            this.TitleBook = titleBook;
+        }
+
+        public void BuyBook()
+        {
+            Console.WriteLine("{0} was written by {1} with {2} pages, it is currently been sold for {3} dollars by {4}. ISBN is {5}",
+                TitleBook, AuthorsBook, PagesBook, PriceBook, PublisherBook, SKU
+                );
+        }
+    }
+
+    public class Airplanes
+    {
+        public string Manufacturer;
+        public string Model;
+        public string Variant;
+        public int Capacity;
+        public int Engines;
+
+        public Airplanes(string manufacturer, string model, string variant, int capacity, int engines)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Variant = variant;
+            this.Capacity = capacity;
+            this.Engines = engines;
+        }
+
+        public void AirShow()
+        {
+            Console.WriteLine("{0} was built by {1} this is it's {2} number. It can hold {3} crew members and has {4} engines!", Model, Manufacturer, Variant, Capacity, Engines);
+        }
+    }
+
+   
+}
+
---
+++ ToDoItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ListItem = new List<ToDoItem>();
+            bool run = true;
+            do
+            {
+                Console.WriteLine("Would you like to make a checklist? Y/N?");
+                string UserAnswer = Console.ReadLine().ToLower();
+                if (UserAnswer == "y")
+                {
+
+                    ToDoItem Chore = new ToDoItem();
+                    Console.WriteLine("Enter an item description.");
+                    Chore.Description = Console.ReadLine();
+
+                    Console.WriteLine("Enter the due date like this 01/30/2020");
+                    Chore.DueDate = Console.ReadLine();
+
+                    Console.WriteLine("then enter a priority level between, low, medium, or high.");
+                    Chore.Priority = Console.ReadLine();
+                    
+                    ListItem.Add(Chore);
+
+                    run = false;
+
+                }
+                else 
+                {
+                    run = true; 
+                };
+
+            } while (!run);
+
+            foreach (var item in ListItem)
+            {
+                Console.WriteLine(item.Description +" "+item.DueDate+" "+item.Priority);
+            };
+
+         }
+    }
+
+    
+
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+
+        public string DueDate { get; set; }
+
+        public string Priority { get; set; }
+
+    }
+
+}
