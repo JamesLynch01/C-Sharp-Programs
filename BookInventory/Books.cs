1d01d6531147ad3cb0693eb30836ceef00e74fce
JamesLynch01 <60361270+JamesLynch01@users.noreply.github.com>
2020-05-17 16:38:04 -0500

    Add files via upload

---
+++ BookInventory/Books.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BookInventory
+{
+    public class Book
+    {
+        public string title { get; set; }
+
+        public string author { get; set; }
+
+        public int id { get; set; }
+
+        //I know normally, that the list of item in my contructor would be lower case
+        //However, my items in the datebase are lower case, so I am just making sure they match
+        //so later I don't have problems.
+        public Book(string Title, string Author)
+        {
+            this.title = Title;
+            this.author = Author;
+        }
+    }
+}
---
+++ BookInventory/BooksIndex.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BookInventory
+{
+    class BooksIndex : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            DirectoryInfo ExecutionDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            DirectoryInfo ProjectBase = ExecutionDirectory.Parent.Parent.Parent;
+
+            string DatabaseFile = Path.Combine(ProjectBase.FullName, "books.db");
+
+            optionsBuilder.UseSqlite("Date Source=" + DatabaseFile);
+        }
+
+        public void Print()
+        {
+            foreach(var book in Books)
+            {
+                Console.WriteLine($"Title: {book.title} Author: {book.author}");
+            }
+        } 
+    }
+}
